6e897eeadc5f3ff4714dd7ea8d489f930e4052d2
0xRektified <bugislife@proton.me>
2026-02-05 13:51:11 +0800

    fat: base test setup , plus swap base logic

--- tests/anchor-amm-q4-25.ts
+++ tests/anchor-amm-q4-25.ts
@@ -1,16 +1,88 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorAmmQ425 } from "../target/types/anchor_amm_q4_25";
+import { getAssociatedTokenAddressSync, createAssociatedTokenAccountInstruction, createMint, mintTo, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 describe("anchor-amm-q4-25", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.anchorAmmQ425 as Program<AnchorAmmQ425>;
+  const initializer = provider.wallet.publicKey;
+  const user = anchor.web3.Keypair.generate();
+  const minter = provider.wallet.publicKey;
+  let configPda = provider.wallet.publicKey;
+  let configBump: number;
+
+  let mintLp: anchor.web3.PublicKey;
+  let mintLpBump: number;
+  
+  let mintX: anchor.web3.PublicKey;
+  let mintY: anchor.web3.PublicKey;
+
+  let userX: anchor.web3.PublicKey;
+  let userY: anchor.web3.PublicKey;
+
+  let vaultX: anchor.web3.PublicKey;
+  let vaultY: anchor.web3.PublicKey;
+
+  let seed = new anchor.BN(1234);
+  let fee = 100;
+
+
+  before(async () => {
+    await provider.connection.requestAirdrop(user.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    mintX = await createMint(provider.connection, provider.wallet.payer, minter, null, 6);
+    mintY = await createMint(provider.connection, provider.wallet.payer, minter, null, 6);
+
+    userX = getAssociatedTokenAddressSync(mintX, user.publicKey);
+    const userXAtaATx = new anchor.web3.Transaction().add(
+      createAssociatedTokenAccountInstruction(provider.wallet.publicKey, userX, user.publicKey, mintX)
+    );
+    await provider.sendAndConfirm(userXAtaATx);
+
+    userY = getAssociatedTokenAddressSync(mintY, user.publicKey);
+    const userYAtaATx = new anchor.web3.Transaction().add(
+      createAssociatedTokenAccountInstruction(provider.wallet.publicKey, userY, user.publicKey, mintY)
+    );
+    await provider.sendAndConfirm(userYAtaATx);
+
+
+    seed = new anchor.BN(1111);
+    [configPda, configBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("config"), seed.toArrayLike(Buffer, "le", 8)],
+      program.programId
+    );
+    [mintLp, mintLpBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("lp"), configPda.toBuffer()],
+      program.programId
+    );
+
+    vaultX = getAssociatedTokenAddressSync(mintX, configPda, true);
+    vaultY = getAssociatedTokenAddressSync(mintY, configPda, true);
+  })
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize(
+      seed,
+      fee,
+      configPda,
+    ).accountsStrict({
+      initializer: initializer,
+      mintX: mintX,
+      mintY: mintY,
+      mintLp: mintLp,
+      vaultX: vaultX,
+      vaultY: vaultY,
+      config: configPda,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
